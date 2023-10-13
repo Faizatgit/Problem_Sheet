92228f7ea8367ba9a01d6c08a815ec03d483c328
Faizan <mohammadfaizan6867@gmail.com>
2023-10-12 23:30:14 -0700

    better approach

---
+++ Zero_matrix/betterApproach.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix , int n, int m)
+{
+    int row[n] = {0};
+    int col[m] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+        {
+            row[i] = 1;
+            col[j] = 1;
+            
+        }
+        }
+        
+        
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (row[i] || col[j])
+            {
+                matrix[i][j] = 0;
+            }
+            
+        }
+        
+    }
+    
+    return matrix;
+    
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    int n = matrix.size();
+    int m = matrix[0].size();
+    vector<vector<int>> res = zeroMatrix(matrix , n , m);
+    for (auto x :res )
+    {
+        for(auto i: x)
+        {
+            cout<<i<<" ";
+        }cout<<endl;
+    }
+
+    return 0;
+}
