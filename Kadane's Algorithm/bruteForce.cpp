64aee66d0b2a13b7edfaefcbd3dd9365921eb7e8
Faizan <mohammadfaizan6867@gmail.com>
2023-10-14 07:14:40 -0700

    brute force

---
+++ Kadane's Algorithm/bruteForce.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSubarray( int arr[] , int n)
+{
+    int maxi = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int sum = 0;
+            for (int k = i; k<= j; k++)
+            {
+                sum += arr[k];
+            }
+            maxi = max(maxi,sum);
+        }
+        
+    }
+
+    return maxi;
+    
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int res =  maxSubarray(arr , n);
+    cout<<"Maximum sum of sub array: "<<res<<endl;
+    return 0;
+
+}
