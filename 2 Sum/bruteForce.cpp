7b7caa20e7fc6b6be72ca444c4f3dee6a0c0b08d
Faizan <mohammadfaizan6867@gmail.com>
2023-10-24 01:44:53 -0700

    Another commit

--- 2 Sum/bruteForce.cpp
+++ 2 Sum/bruteForce.cpp
@@ -2,29 +2,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string twoSumProb(vector<int> &arr, int target)
-{
-    int n = arr.size();
+// string twoSumProb(vector<int> &arr, int target)
+// {
+//     int n = arr.size();
 
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (arr[i] + arr[j] == target)
+//             {
+//                 return "Yes";
+//             }
+//         }
+//     }
+
+//     return "No";
+// }
+
+// int main()
+// {
+//     vector<int> arr = {2, 6, 5, 8, 11};
+//     int target = 14;
+//     string res = twoSumProb(arr, target);
+//     cout << res << endl;
+//     return 0;
+// }
+
+vector<int> twoSumProb(vector<int> &arr, int target)
+{
+    vector<int> ans;
+
+    int n = arr.size();
     for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        for (int j = i+1; j < n; j++)
         {
-            if (arr[i] + arr[j] == target)
+            if(arr[i]+arr[j] == target)
             {
-                return "Yes";
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
             }
         }
+        
     }
-
-    return "No";
+    
+    return {-1,-1};
 }
 
 int main()
 {
     vector<int> arr = {2, 6, 5, 8, 11};
     int target = 14;
-    string res = twoSumProb(arr, target);
-    cout << res << endl;
+    vector<int> res = twoSumProb(arr,target);
+    cout<<"Indexs are: "<<endl;
+    for(auto i:res)
+    {
+        cout<<i<<" ";
+    }cout<<endl;
+
     return 0;
 }
