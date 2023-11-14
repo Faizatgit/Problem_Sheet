40c57c155772e0844e3474f1463c99f1b31b2b80
Faizan <mohammadfaizan6867@gmail.com>
2023-11-14 15:57:02 +0530

    Four sum

---
+++ 4 sum/better.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<algorithm>
+using namespace std;
+
+vector<vector<int>> foursum(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            set<long long> hashset;
+            for (int k = j+1; k < n; k++)
+            {
+                long long sum = nums[i] + nums[j];
+                sum += nums[k];
+                long long fourth = target - sum;
+                if (hashset.find(fourth) != hashset.end())
+                {
+                    vector<int> temp = {nums[i] , nums[j] , nums[k] , (int)(fourth)};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                }
+
+                hashset.insert(nums[k]);
+            }
+            
+        }
+        
+    }
+    vector<vector<int>> ans(st.begin(),st.end());
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+    int target = 9;
+    vector<vector<int>> res = foursum(nums,target);
+    cout<<"The result is : "<<endl;
+    for(auto i:res)
+    {
+        cout<<'[';
+        for(auto it:i)
+        {
+            cout<<it<<" ";
+        }
+        cout<<"]";
+    }cout<<endl;
+
+    return 0;
+}
