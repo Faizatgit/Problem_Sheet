6433ca6a92ce1dc5f342e26782ba47be23901da9
Faizan <mohammadfaizan6867@gmail.com>
2024-01-01 20:35:23 +0530

    Another commit

---
+++ 3 sum/bruteforce.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> triplet(vector<int> &nums)
+{
+    int n = nums.size();
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; j < n; j++)
+            {
+                if (nums[i] + nums[j] + nums[k] == 0)
+                {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+    vector<vector<int>> res(st.begin(), st.end());
+    return res;
+}
+
+int main()
+{
+    vector<int> arr = {-1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    vector<vector<int>> ans = triplet(arr);
+    for (auto it : ans)
+    {
+        cout << "[";
+        for (auto i : it)
+        {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
