875eac3ee47d84fc954d4bec68209f34952b9960
Faizan <mohammadfaizan6867@gmail.com>
2024-01-03 16:11:21 +0530

    Another commit

--- 3 sum/optimal.cpp
+++ 3 sum/optimal.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
 using namespace std;
 
 vector<vector<int>> triplet(vector<int> &nums)
 {
     int n = nums.size();
-    set<vector<int>> st;
-
+    vector<vector<int>> res;
+    sort(nums.begin(), nums.end());
     for (int i = 0; i < n; i++)
     {
-        set<int> hashset;
-        for (int j = i + 1; j < n; j++)
+        if (i != 0 && nums[i] == nums[i - 1])
+            continue;
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k)
         {
-            int third = -(nums[i] + nums[j]);
-            if (hashset.find(third) != hashset.end())
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0)
             {
-                vector<int> temp = {nums[i], nums[j], third};
-                sort(temp.begin(), temp.end());
-                st.insert(temp);
+                j++;
             }
-            hashset.insert(nums[j]);
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+            {
+                vector<int> temp = {nums[i], nums[j], nums[k]};
+                res.push_back(temp);
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j + 1]) j++;
+                while (j < k && nums[k] == nums[k - 1]) k--;
+            }
         }
     }
-
-    vector<vector<int>> ans(st.begin(), st.end());
-    return ans;
+    return res;
 }
 
 int main()
