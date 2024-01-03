b1c20f51316f99dbc9640ac21637a0b37fca3a60
Faizan <mohammadfaizan6867@gmail.com>
2024-01-03 15:53:33 +0530

    Another commit

--- 3 sum/optimal.cpp
+++ 3 sum/optimal.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <set>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 vector<vector<int>> triplet(vector<int> &nums)
 {
     int n = nums.size();
     set<vector<int>> st;
+
     for (int i = 0; i < n; i++)
     {
+        set<int> hashset;
         for (int j = i + 1; j < n; j++)
         {
-            for (int k = j + 1; k < n; k++)
+            int third = -(nums[i] + nums[j]);
+            if (hashset.find(third) != hashset.end())
             {
-                if (nums[i] + nums[j] + nums[k] == 0)
-                {
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
-                }
+                vector<int> temp = {nums[i], nums[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
             }
+            hashset.insert(nums[j]);
         }
     }
-    vector<vector<int>> res(st.begin(), st.end());
-    return res;
+
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
 }
 
 int main()
 {
     vector<int> arr = {-1, 0, 1, 2, -1, -4};
-    int n = arr.size();
     vector<vector<int>> ans = triplet(arr);
     for (auto it : ans)
     {
